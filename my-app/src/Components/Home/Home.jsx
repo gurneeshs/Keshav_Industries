df0377357531e02943a586e95959cce8c8db7ebf
Gurneesh <singhgurneesh2022@gmail.com>
2024-08-06 11:31:42 +0530

    test

--- my-app/src/Components/Home/Home.jsx
+++ my-app/src/Components/Home/Home.jsx
@@ -144,21 +144,19 @@ const Home = () => {
 
 
       <Container fluid className="expoter">
-        
-        <Container>
-        <div className="row">
-          <div className="col-6 expoter_right">
-          <Image src={logo_removebg} alt="Logo" className="home-expoter-logo" />
-            <h1 className="">Trusted Exorter of <br />
-              Products</h1>
-            <p>We are leading manufacturer & Exporter of Soya Refined Oil, Mustard Oil, Non-GMO Lecithin (Soya, Sunflower & Rice)</p>
-          </div>
 
-          
-          <div className="col-6">
-          {/* <Slider /> */}
+        <Container>
+          <div className="row">
+            <div className="col-6 expoter_right">
+              <Image src={logo_removebg} alt="Logo" className="home-expoter-logo" />
+              <h1 className="">Trusted Exorter of <br />
+                Products</h1>
+              <p>We are leading manufacturer & Exporter of Soya Refined Oil, Mustard Oil, Non-GMO Lecithin (Soya, Sunflower & Rice)</p>
+            </div>
+            <div className="col-6">
+              <Slider />
+            </div>
           </div>
-        </div>
         </Container>
 
       </Container>
@@ -167,7 +165,7 @@ const Home = () => {
         <div className="row text-center categories-row-first">
           <h1 className="">Certifications</h1>
           <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           </p>
         </div>
       </Container>
@@ -179,15 +177,15 @@ const Home = () => {
       <Container >
         <div className="row text-center categories-row-first">
           <h1 className="">Trusted Exporter of Quality Spices <br />
-          & Non-GMO Lecithin</h1>
+            & Non-GMO Lecithin</h1>
           <p>
-          We are the leading exporter of Non-GMO Soya Lecithin, Lyso Lecithin, Sunflower Lecithin, Rice  <br />
-Lecithin as well as variety of Quality Spices to more than 35 countries since more than a decade.
+            We are the leading exporter of Non-GMO Soya Lecithin, Lyso Lecithin, Sunflower Lecithin, Rice  <br />
+            Lecithin as well as variety of Quality Spices to more than 35 countries since more than a decade.
           </p>
         </div>
       </Container>
 
-      
+
       {/* Map Video */}
       <Container>
 
@@ -196,93 +194,93 @@ Lecithin as well as variety of Quality Spices to more than 35 countries since mo
 
       <Container>
         <main>
-        <div className="row">
-        <div class="container-xxl py-5">
-    <div class="container py-5">
-        <div class="testimonial-text g-5">
-            <div class="wow fadeIn" data-wow-delay="0.1s">
-                <button class="btn btn-sm border rounded-pill text-primary px-3 mb-3">Testimonial</button>
-                <h1 class="mb-4">LambdaTest Reviews</h1>
+          <div className="row">
+            <div class="container-xxl py-5">
+              <div class="container py-5">
+                <div class="testimonial-text g-5">
+                  <div class="wow fadeIn" data-wow-delay="0.1s">
+                    <button class="btn btn-sm border rounded-pill text-primary px-3 mb-3">Testimonial</button>
+                    <h1 class="mb-4">LambdaTest Reviews</h1>
                     <p class="mb-4">Our Unified Testing Cloud enables you to deliver world class digital experience with quality releases and help accelerate your release velocity.</p>
                     <a class="btn btn-primary rounded-pill px-4" href="https://www.lambdatest.com/reviews">Contact Sales</a>
-            </div>
-        
+                  </div>
 
-      <section class="carousel-landmark wow fadeIn" data-wow-delay="0.5s">
-        <div id="carouselExampleCaptions" class="carousel slide testimonial-carousel border-start border-primary">
-          
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-              <div class="carousel-item carousel-item-1 active">
-                        <div class="testimonial-item ps-5">
+
+                  <section class="carousel-landmark wow fadeIn" data-wow-delay="0.5s">
+                    <div id="carouselExampleCaptions" class="carousel slide testimonial-carousel border-start border-primary">
+
+                      <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                      </div>
+                      <div class="carousel-inner">
+                        <div class="carousel-item carousel-item-1 active">
+                          <div class="testimonial-item ps-5">
                             <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
                             <p class="fs-4 textimal-text">Super top notch customer support from <em>@lambdatesting</em> - just throwing it out there if you're looking for a decent browser testing platform, they get my full double thumbs up. Thumbs upThumbs up</p>
                             <div class="d-flex align-items-center">
-                                {/* <Image class="img-fluid flex-shrink-0 rounded-circle review_img" src="https://user-images.githubusercontent.com/78242022/266013790-4d674d96-a311-47c3-9b7c-03feaa36c948.png"/> */}
-                                <div class="ps-3">
-                                    <h5 class="mb-1">Ben Pritchard</h5>
-                                    <span class="at">@yesiamben</span>
-                                </div>
+                              {/* <Image class="img-fluid flex-shrink-0 rounded-circle review_img" src="https://user-images.githubusercontent.com/78242022/266013790-4d674d96-a311-47c3-9b7c-03feaa36c948.png"/> */}
+                              <div class="ps-3">
+                                <h5 class="mb-1">Ben Pritchard</h5>
+                                <span class="at">@yesiamben</span>
+                              </div>
                             </div>
+                          </div>
                         </div>
-                    </div>
 
-            
-            <div class="carousel-item carousel-item-2">
-                        <div class="testimonial-item ps-5">
+
+                        <div class="carousel-item carousel-item-2">
+                          <div class="testimonial-item ps-5">
                             <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
                             <p class="fs-4 textimal-text"><em>@lambdatesting</em> is fantastic. Cross browser and device testingtesting frustration is minimized. You can't get rid of clients that need ie11 nor can you own every device but lambda test bridge that gap.</p>
                             <div class="d-flex align-items-center">
-                                {/* <Image class="img-fluid flex-shrink-0 rounded-circle review_img" src="https://user-images.githubusercontent.com/78242022/266013776-40ac50f1-31f8-4250-acb2-05f16d683baa.png"/> */}
-                                <div class="ps-3">
-                                    <h5 class="mb-1">Mat Gargano</h5>
-                                    <span class="at">@matgargano</span>
-                                </div>
+                              {/* <Image class="img-fluid flex-shrink-0 rounded-circle review_img" src="https://user-images.githubusercontent.com/78242022/266013776-40ac50f1-31f8-4250-acb2-05f16d683baa.png"/> */}
+                              <div class="ps-3">
+                                <h5 class="mb-1">Mat Gargano</h5>
+                                <span class="at">@matgargano</span>
+                              </div>
                             </div>
+                          </div>
                         </div>
-                    </div>
 
-            
-            <div class="carousel-item carousel-item-2">
-                    <div class="testimonial-item ps-5">
-                        <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
-                        <p class="fs-4 textimal-text">second-day using <em>@lambdatesting</em> and it's already proven itself a lot faster than Cross Browser Testing and BrowserStack, at half the price! bargain</p>
-                        <div class="d-flex align-items-center">
-                            {/* <Image class="img-fluid flex-shrink-0 rounded-circle review_img" src="https://user-images.githubusercontent.com/78242022/266013762-54201d6a-0923-4969-948c-790dec804253.png"/> */}
-                            <div class="ps-3">
+
+                        <div class="carousel-item carousel-item-2">
+                          <div class="testimonial-item ps-5">
+                            <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
+                            <p class="fs-4 textimal-text">second-day using <em>@lambdatesting</em> and it's already proven itself a lot faster than Cross Browser Testing and BrowserStack, at half the price! bargain</p>
+                            <div class="d-flex align-items-center">
+                              {/* <Image class="img-fluid flex-shrink-0 rounded-circle review_img" src="https://user-images.githubusercontent.com/78242022/266013762-54201d6a-0923-4969-948c-790dec804253.png"/> */}
+                              <div class="ps-3">
                                 <h5 class="mb-1">Matthew Bryson</h5>
                                 <span class="at">@mbrysonuk</span>
+                              </div>
                             </div>
+                          </div>
                         </div>
-                    </div>
-                </div>
-                
-                
-                <div class="control-btn">
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                      <i class="fa-solid fa-arrow-left"></i>
-                      <span class="carousel-control-prev-icon visually-hidden" aria-hidden="true"></span>
-                      <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                      <i class="fa-solid fa-arrow-right"></i>
-                
-                      <span class="carousel-control-next-icon visually-hidden" aria-hidden="true"></span>
-                      <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
 
-        </div>
-    </section>
-</div>
-</div>
-</div>
-        </div>
+
+                        <div class="control-btn">
+                          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <i class="fa-solid fa-arrow-left"></i>
+                            <span class="carousel-control-prev-icon visually-hidden" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                          </button>
+                          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <i class="fa-solid fa-arrow-right"></i>
+
+                            <span class="carousel-control-next-icon visually-hidden" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                          </button>
+                        </div>
+                      </div>
+
+                    </div>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
       </Container>
 
