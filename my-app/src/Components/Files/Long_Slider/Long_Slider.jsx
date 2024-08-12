46cd815f37c94e95a61484c281774383fc1116c1
Gurneesh <singhgurneesh2022@gmail.com>
2024-08-12 22:24:18 +0530

    database

--- my-app/src/Components/Files/Long_Slider/Long_Slider.jsx
+++ my-app/src/Components/Files/Long_Slider/Long_Slider.jsx
@@ -1,6 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
 import Image from "react-bootstrap/Image";
-import test from "../../Images/keshav_all_products.jpg"
+import test from "../../Images/keshav_all_product.jpg"
 
 function Long_Slider() {
   return (
