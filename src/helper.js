aac0519b88d1e50bd389c0b42bab6beb43843f50
Gurneesh <singhgurneesh2022@gmail.com>
2024-11-25 11:31:48 +0530

    up

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'http://localhost:5000'
+export const BASE_URL = 'http://localhost:5000'
 // export const BASE_URL = 'https://razorpay-server-henna.vercel.app'
 
 // export const BASE_URL = 'https://razorpayserver-4g2y.onrender.com';
-export const BASE_URL = 'https://razorpayserver-production.up.railway.app';
+// export const BASE_URL = 'https://razorpayserver-production.up.railway.app';
