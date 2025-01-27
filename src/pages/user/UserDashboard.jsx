2f9e24093864e0d59455f529c69aeb262c40b748
gurneeshs <singhgurneesh2022@gmail.com>
2025-01-27 21:46:48 +0530

    update

--- src/pages/user/UserDashboard.jsx
+++ src/pages/user/UserDashboard.jsx
@@ -156,7 +156,7 @@ const UserDashboard = () => {
               {/* User Info */}
               <div className="bg-white p-4 rounded-sm shadow-md flex items-center space-x-4">
                 <div className="w-12 h-12 flex items-center justify-center">
-                  {userObject?.gender == 'male' ? <img src={user_logo_male} alt="Logo" /> : <img src={user_logo_female} alt="Logo" />
+                  {userObject?.gender == 'Male' ? <img src={user_logo_male} alt="Logo" /> : <img src={user_logo_female} alt="Logo" />
                   }
                 </div>
                 <div>
